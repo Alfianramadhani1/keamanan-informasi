2704b2699b4e919d5dda7a2893c16d484dceaccd
Alfianramadhani1 <146942648+Alfianramadhani1@users.noreply.github.com>
2025-05-20 20:47:07 +0700

    Create listfakultas.php

---
+++ Fakultasresource/pages/listfakultas.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Filament\Resources\FacultyResource\Pages;
+
+use App\Filament\Resources\FacultyResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListFaculties extends ListRecords
+{
+    protected static string $resource = FacultyResource::class;
+}
